8ba06169da6249920d38158380d89cf146d525b2
SirMathhman <SirMathhman@gmail.com>
2019-12-02 12:26:13 -0500

    Add test

---
+++ src/test/java/com/meti/StringLexerStateTest.java
@@ -0,0 +1,20 @@
+package com.meti;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringLexerStateTest {
+
+    @Test
+    void next() {
+    }
+
+    @Test
+    void compute() {
+    }
+
+    @Test
+    void hasMoreTokens() {
+    }
+}
