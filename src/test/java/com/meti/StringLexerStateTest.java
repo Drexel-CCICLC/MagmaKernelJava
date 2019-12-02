b94f3b03c7944f365c553d5b322507e5e9f148c5
SirMathhman <SirMathhman@gmail.com>
2019-12-02 12:36:38 -0500

    Implement test method for next

--- src/test/java/com/meti/StringLexerStateTest.java
+++ src/test/java/com/meti/StringLexerStateTest.java
@@ -1,13 +1,22 @@
 package com.meti;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+
+import static java.util.Collections.singleton;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringLexerStateTest {
 
     @Test
     void next() {
+        LexerState state = new StringLexerState("test");
+        Optional<Token<?>> expected = Optional.of(Mockito.mock(Token.class));
+        Optional<Token<?>> actual = state.next(singleton(lexerState -> expected));
+        assertEquals(expected, actual);
+        assertEquals("e", state.compute());
     }
 
     @Test
