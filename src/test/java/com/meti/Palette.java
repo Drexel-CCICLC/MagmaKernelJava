c639d47c877820a5d19e13ffbc2fa09a343ad477
SirMathhman <sirmathhman@gmail.com>
2019-12-17 16:16:25 -0500

    Extract IfTest

--- src/test/java/com/meti/Palette.java
+++ src/test/java/com/meti/Palette.java
@@ -5,15 +5,5 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Palette extends CompileTest {
-	@Test
-	void testIf() {
-		String result = compiler.compile("if(true){val x = 10;}");
-		assertEquals("if(true){var a0=10;}", result);
-	}
 
-	@Test
-	void testIfElse() {
-		String result = compiler.compile("if(true){val x = 10;};else{val y = 20;}");
-		assertEquals("if(true){var a0=10;}else{var b1=20;}", result);
-	}
 }
