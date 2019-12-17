eb9858598a2724e707623b647cd60fba45f0321f
SirMathhman <sirmathhman@gmail.com>
2019-12-17 12:15:55 -0500

    Add Compiler

---
+++ src/main/java/com/meti/Compiler.java
@@ -0,0 +1,8 @@
+package com.meti;
+
+public class Compiler {
+
+	String compile() {
+		return "10";
+	}
+}
---
+++ src/test/java/com/meti/Palette.java
@@ -0,0 +1,12 @@
+package com.meti;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Palette {
+	@Test
+	void describe() {
+		assertEquals("10", new Compiler().compile());
+	}
+}
