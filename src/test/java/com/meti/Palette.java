1502071ff5d59504f509597d8f1c6e4768d7f1e7
SirMathhman <sirmathhman@gmail.com>
2019-12-18 12:24:41 -0500

    Extract NativeTest

--- src/test/java/com/meti/Palette.java
+++ src/test/java/com/meti/Palette.java
@@ -6,15 +6,5 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Palette extends CompileTest {
-	@Test
-	void nativeMethod() {
-		String result = compiler.compile("native val writeString = (value)");
-		assertTrue(result.isBlank());
-	}
 
-	@Test
-	void nativeInvoke() {
-		String result = compiler.compile("native val writeInt = (value); val result = writeInt(10);");
-		assertEquals("var b1=writeInt(10);", result);
-	}
 }
