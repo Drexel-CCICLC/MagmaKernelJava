fa42096bc7adc67eacabf7d73dad0fc48486223b
SirMathhman <sirmathhman@gmail.com>
2019-12-19 14:06:17 -0500

    Add native files

---
+++ out/native.js
@@ -0,0 +1,15 @@
+function Array_size(size) {
+    return new Array(size);
+}
+
+function Array_length(array) {
+    return array.length;
+}
+
+function Array_get(array, index) {
+    return array[index];
+}
+
+function Array_set(array, index, value) {
+    array[index] = value;
+}
