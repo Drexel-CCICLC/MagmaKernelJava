668f4a58d1dc92bdcff149cc2c729a563b66e6cb
SirMathhman <SirMathhman@gmail.com>
2020-01-07 14:36:05 -0500

    Add content test

--- src/test/java/com/meti/BlockTest.java
+++ src/test/java/com/meti/BlockTest.java
@@ -12,6 +12,21 @@ public class BlockTest extends CompileTest {
 	}
 
 	@Test
+	void multiple() {
+		String result = compileAll("val other = () => void :{\n" +
+				"};\n" +
+				"\n" +
+				"val doSomething = () => void :{\n" +
+				"     other();\n" +
+				"}");
+		assertEquals("void other$(){}" +
+				"void(*other)()=&other$;" +
+				"void doSomething$(){other();}" +
+				"void(*doSomething)()=&doSomething$;" +
+				"int main(){return 0;}", result);
+	}
+
+	@Test
 	void test() {
 		String result = compileOnly("{}");
 		assertEquals("{}", result);
