4b1c295dadc6ecccd446a01aa9f9ae825d551a21
SirMathhman <sirmathhman@gmail.com>
2020-01-16 12:45:31 -0500

    Add parseMultiple

--- src/main/java/com/meti/Parser.java
+++ src/main/java/com/meti/Parser.java
@@ -1,7 +1,15 @@
 package com.meti;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Optional;
 
 public interface Parser {
-	Optional<Node> parse(String value, Compiler compiler);
+	default Optional<Node> parse(String value, Compiler compiler) {
+		return parseMultiple(value, compiler).stream().findFirst();
+	}
+
+	default Collection<Node> parseMultiple(String value, Compiler compiler) {
+		return Collections.emptySet();
+	}
 }
