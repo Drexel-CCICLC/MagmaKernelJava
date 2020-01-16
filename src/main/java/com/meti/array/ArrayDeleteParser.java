d12f5cd6b31f72ee8dfcba43ca3d6848b68adf90
SirMathhman <sirmathhman@gmail.com>
2020-01-16 18:57:00 -0500

    Remove annotation

--- src/main/java/com/meti/array/ArrayDeleteParser.java
+++ src/main/java/com/meti/array/ArrayDeleteParser.java
@@ -3,28 +3,26 @@ package com.meti.array;
 import com.meti.Compiler;
 import com.meti.Node;
 import com.meti.Parser;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 import java.util.Collections;
 
 public class ArrayDeleteParser implements Parser {
-    private static final String HEADER = "delete ";
+	private static final String HEADER = "delete ";
 
-    @NotNull
-    private Node buildNode(Compiler compiler, String trim) {
-        String valueString = trim.substring(HEADER.length());
-        Node valueNode = compiler.parseSingle(valueString);
-        return new ArrayDeleteNode(valueNode);
-    }
+	@Override
+	public Collection<Node> parseMultiple(String value, Compiler compiler) {
+		String trim = value.trim();
+		if (trim.startsWith(HEADER)) {
+			Node node = buildNode(compiler, trim);
+			return Collections.singleton(node);
+		}
+		return Collections.emptySet();
+	}
 
-    @Override
-    public Collection<Node> parseMultiple(String value, Compiler compiler) {
-        String trim = value.trim();
-        if (trim.startsWith(HEADER)) {
-            Node node = buildNode(compiler, trim);
-            return Collections.singleton(node);
-        }
-        return Collections.emptySet();
-    }
+	private Node buildNode(Compiler compiler, String trim) {
+		String valueString = trim.substring(HEADER.length());
+		Node valueNode = compiler.parseSingle(valueString);
+		return new ArrayDeleteNode(valueNode);
+	}
 }
