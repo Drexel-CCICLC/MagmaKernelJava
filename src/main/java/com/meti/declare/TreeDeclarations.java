a9d4677defb2d80c529b75dd825b86a75dd1b94b
SirMathhman <sirmathhman@gmail.com>
2020-01-17 18:27:34 -0500

    Replace with reduction

--- src/main/java/com/meti/declare/TreeDeclarations.java
+++ src/main/java/com/meti/declare/TreeDeclarations.java
@@ -8,73 +8,67 @@ import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class TreeDeclarations implements Declarations {
-    private final Declaration root = new TreeDeclaration("root", null, false, null);
-    private final Stack<String> stack = new Stack<>();
+	private final Declaration root = new TreeDeclaration("root", null, false, null);
+	private final Stack<String> stack = new Stack<>();
 
-    @Override
-    public Declaration root() {
-        return root;
-    }
+	@Override
+	public Declaration root() {
+		return root;
+	}
 
-    @Override
-    public Node define(String name, Type type, Supplier<? extends Node> action) {
-        define(name, type, false);
-        stack.push(name);
-        Node result = action.get();
-        stack.pop();
-        return result;
-    }
+	@Override
+	public Node define(String name, Type type, Supplier<? extends Node> action) {
+		define(name, type, false);
+		stack.push(name);
+		Node result = action.get();
+		stack.pop();
+		return result;
+	}
 
-    @Override
-    public void define(String name, Type type, boolean isParameter) {
-        current().define(name, type, isParameter);
-    }
+	@Override
+	public void define(String name, Type type, boolean isParameter) {
+		current().define(name, type, isParameter);
+	}
 
-    @Override
-    public Declaration current() {
-        return absolute(stack);
-    }
+	@Override
+	public Declaration current() {
+		return absolute(stack);
+	}
 
-    private Declaration absolute(Iterable<String> values) {
-        Declaration current = root;
-        for (String value : values) {
-            current = current.child(value)
-                    .orElseThrow();
-        }
-        return current;
-    }
+	private Declaration absolute(Collection<String> values) {
+        return values.stream().reduce(root,
+				(declaration, s) -> declaration.child(s).orElseThrow(),
+				(declaration, declaration2) -> declaration2);
+	}
 
-    @Override
-    public Optional<Declaration> relative(String value) {
-        Deque<String> reverseStack = new LinkedList<>(stack);
-        while (!reverseStack.isEmpty()) {
-            Optional<Declaration> optional = absolute(reverseStack).child(value);
-            if (optional.isPresent()) {
-                return optional;
-            } else {
-                reverseStack.pollLast();
-            }
-        }
-        return root.child(value);
-    }
+	@Override
+	public Optional<Declaration> relative(String value) {
+		Deque<String> reverseStack = new LinkedList<>(stack);
+		while (!reverseStack.isEmpty()) {
+			Optional<Declaration> optional = absolute(reverseStack).child(value);
+			if (optional.isPresent()) return optional;
+			reverseStack.pollLast();
+		}
+		return root.child(value);
+	}
 
-    @Override
-    public Stream<Declaration> stream() {
-        Collection<Declaration> declarations = new ArrayList<>();
-        Deque<String> reverseStack = new LinkedList<>(stack);
-        while (!reverseStack.isEmpty()) {
-            declarations.add(absolute(reverseStack));
-            reverseStack.pollLast();
-        }
-        declarations.add(root);
-        return declarations.stream();
-    }
+	@Override
+	public Stream<Declaration> stream() {
+		Collection<Declaration> declarations = new ArrayList<>();
+		Deque<String> reverseStack = new LinkedList<>(stack);
+		while (!reverseStack.isEmpty()) {
+			declarations.add(absolute(reverseStack));
+			reverseStack.pollLast();
+		}
+		declarations.add(root);
+		return declarations.stream();
+	}
 
-    @Override
-    public void define(String name, Type type, Runnable action) {
-        define(name, type, false);
-        stack.push(name);
-        action.run();
-        stack.pop();
-    }
+	@Override
+	public void define(String name, Type type, Runnable action) {
+		define(name, type, false);
+		stack.push(name);
+		action.run();
+		stack.pop();
+	}
 }
