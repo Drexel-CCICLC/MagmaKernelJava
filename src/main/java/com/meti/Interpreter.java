45529f7e219bb7a17fb24d10c97f23a62c61a586
SirMathhman <sirmathhman@gmail.com>
2020-01-16 21:59:46 -0500

    Make weaker

--- src/main/java/com/meti/Interpreter.java
+++ src/main/java/com/meti/Interpreter.java
@@ -3,7 +3,7 @@ package com.meti;
 import java.io.IOException;
 import java.util.Optional;
 
-public interface Interpreter {
+interface Interpreter {
 	Optional<String> parse(String content);
 
 	String run(String content) throws IOException, InterruptedException;
--- src/test/java/com/meti/ArrayContentTest.java
+++ src/test/java/com/meti/ArrayContentTest.java
@@ -1,6 +1,5 @@
 package com.meti;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
