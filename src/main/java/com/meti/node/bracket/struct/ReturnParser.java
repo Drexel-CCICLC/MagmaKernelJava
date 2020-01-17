e9ce796dbbcf77c33b419e1fc9025051efbe0b7f
SirMathhman <sirmathhman@gmail.com>
2020-01-17 18:45:46 -0500

    Make ReturnParser functional

--- src/main/java/com/meti/node/bracket/struct/ReturnParser.java
+++ src/main/java/com/meti/node/bracket/struct/ReturnParser.java
@@ -9,18 +9,17 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class ReturnParser implements Parser {
-    private Optional<Node> parse(String value, Compiler compiler) {
-        String trim = value.trim();
-        if (trim.startsWith("return ")) {
-            String valueString = trim.substring(7);
-            Node node = compiler.parseSingle(valueString);
-            return Optional.of(new ReturnNode(node));
-        }
-        return Optional.empty();
-    }
-
 	@Override
 	public Collection<Node> parseMultiple(String value, Compiler compiler) {
 		return parse(value, compiler).stream().collect(Collectors.toSet());
+	}
+
+	private Optional<Node> parse(String value, Compiler compiler) {
+		return Optional.of(value)
+				.map(String::trim)
+				.filter(trim -> trim.startsWith("return "))
+				.map(trim -> trim.substring(7))
+				.map(compiler::parseSingle)
+				.map(ReturnNode::new);
 	}
 }
