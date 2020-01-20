611ca688130fa153e5659462f746b8551aba8a29
SirMathhman <sirmathhman@gmail.com>
2020-01-20 18:13:29 -0500

    Add test for TreeDeclarations.current

--- src/test/java/com/meti/declare/TreeDeclarationsTest.java
+++ src/test/java/com/meti/declare/TreeDeclarationsTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 class TreeDeclarationsTest {
 
@@ -15,5 +15,47 @@ class TreeDeclarationsTest {
 		Declaration expected = declarations.define("main", IntType.INSTANCE);
 		Declaration actual = declarations.absolute(Collections.singleton("main"));
 		assertSame(expected, actual);
+	}
+
+	@Test
+	void current() {
+		Declarations declarations = new TreeDeclarations();
+		declarations.isRoot(declarations.current());
+	}
+
+	@Test
+	void define() {
+	}
+
+	@Test
+	void isCurrent() {
+	}
+
+	@Test
+	void isRoot() {
+	}
+
+	@Test
+	void parentOf() {
+	}
+
+	@Test
+	void relative() {
+	}
+
+	@Test
+	void stream() {
+	}
+
+	@Test
+	void testDefine() {
+	}
+
+	@Test
+	void testDefine1() {
+	}
+
+	@Test
+	void testDefine2() {
 	}
 }
