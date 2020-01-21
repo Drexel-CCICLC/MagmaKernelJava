171ffdd0885bfe2e4887e2dbdd0b7038058dcb03
SirMathhman <sirmathhman@gmail.com>
2020-01-20 21:24:31 -0500

    Add default method

--- src/main/java/com/meti/node/Parser.java
+++ src/main/java/com/meti/node/Parser.java
@@ -3,7 +3,16 @@ package com.meti.node;
 import com.meti.compile.Compiler;
 
 import java.util.Collection;
+import java.util.Optional;
 
 public interface Parser {
+	default Optional<Node> parse(String value, Compiler compiler) {
+		Collection<Node> nodes = parseMultiple(value, compiler);
+		Object[] array = nodes.toArray();
+		return 1 == array.length ?
+				Optional.of((Node) array[0]) :
+				Optional.empty();
+	}
+
 	Collection<Node> parseMultiple(String value, Compiler compiler);
 }
