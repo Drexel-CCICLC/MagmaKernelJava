203482add1d3b352791f795e3620a6f844c24caf
SirMathhman <sirmathhman@gmail.com>
2020-01-20 21:52:28 -0500

    Remove bad test

--- src/test/java/com/meti/node/value/compound/variable/VariableParserTest.java
+++ src/test/java/com/meti/node/value/compound/variable/VariableParserTest.java
@@ -17,17 +17,4 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class VariableParserTest {
 
-	@Test
-	void parse() {
-		Declarations declarations = new TreeDeclarations();
-		declarations.define("a", create()
-						.withReturnType(IntType.INSTANCE)
-						.withParameter(IntType.INSTANCE)
-						.build(),
-				() -> declarations.define(Parameter.of("value", AnyType.INSTANCE)));
-		Parser parser = new VariableParser(declarations);
-		Optional<Node> nodes = parser.parse("value", null);
-		assertFalse(nodes.isEmpty());
-		assertEquals("*(int*)a_[0]", nodes.get().render());
-	}
 }
