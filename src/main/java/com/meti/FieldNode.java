3b2d61c859597b5a08857fc1e4a0271197e694a2
SirMathhman <sirmathhman@gmail.com>
2020-01-25 14:06:38 -0500

    Add FieldNode

---
+++ src/main/java/com/meti/FieldNode.java
@@ -0,0 +1,16 @@
+package com.meti;
+
+public class FieldNode implements Node {
+	private final Node instance;
+	private final String name;
+
+	public FieldNode(Node instance, String name) {
+		this.instance = instance;
+		this.name = name;
+	}
+
+	@Override
+	public String render() {
+		return instance.render() + "." + name;
+	}
+}
