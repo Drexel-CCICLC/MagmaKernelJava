1dd2310f53d4aafbeda8bbf9d43be3d0c28509d2
SirMathhman <sirmathhman@gmail.com>
2020-01-25 13:33:27 -0500

    Add Invocations

---
+++ src/main/java/com/meti/VoidInvocationNode.java
@@ -0,0 +1,22 @@
+package com.meti;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class VoidInvocationNode implements Node {
+	private final List<? extends Node> arguments;
+	private final Node caller;
+
+	public VoidInvocationNode(Node caller, List<? extends Node> arguments) {
+		this.caller = caller;
+		this.arguments = arguments;
+	}
+
+	@Override
+	public String render() {
+		String joinedArgs = arguments.stream()
+				.map(Node::render)
+				.collect(Collectors.joining(","));
+		return caller.render() + "(" + joinedArgs + ");";
+	}
+}
---
+++ src/test/java/com/meti/InvocationNodeTest.java
@@ -0,0 +1,18 @@
+package com.meti;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class InvocationNodeTest {
+
+	@Test
+	void render() {
+		Node a = new VariableNode("a");
+		Node b = new VariableNode("b");
+		Node node = new InvocationNode(a, Collections.singletonList(b));
+		assertEquals("a(b)", node.render());
+	}
+}
