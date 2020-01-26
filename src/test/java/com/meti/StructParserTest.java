fa9003bf872a97302bb141e801d86a27821cea53
SirMathhman <sirmathhman@gmail.com>
2020-01-25 22:34:05 -0500

    Add more comprehensive tests

--- src/test/java/com/meti/StructParserTest.java
+++ src/test/java/com/meti/StructParserTest.java
@@ -9,12 +9,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StructParserTest {
-
 	private Cache cache;
 	private Compiler compiler;
 	private Declarations declarations;
 	private Parser parser;
 	private Resolver resolver;
+
+	@Test
+	void empty() throws ParseException {
+		compiler.parse("val empty =: {}");
+		assertEquals("int _exit=0i;void empty(){}int main(){return _exit;}", cache.render());
+	}
 
 	@Test
 	void parseComplete() throws ParseException {
@@ -38,5 +43,17 @@ class StructParserTest {
 				new IntResolver()
 		);
 		compiler = new Compiler(parser, resolver);
+	}
+
+	@Test
+	void withParam() throws ParseException {
+		compiler.parse("val accept = (Int some) : {}");
+		assertEquals("int _exit=0i;void accept(int some){}int main(){return _exit;}", cache.render());
+	}
+
+	@Test
+	void withTwoParam() throws ParseException {
+		compiler.parse("val accept = (Int one, Int two) : {}");
+		assertEquals("int _exit=0i;void accept(int one,int two){}int main(){return _exit;}", cache.render());
 	}
 }
