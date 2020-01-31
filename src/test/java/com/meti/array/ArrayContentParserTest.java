04a0bc3d53cfb7ae21876ed5680d674b46259107
SirMathhman <SirMathhman@gmail.com>
2020-01-31 14:34:14 -0500

    Add relevant preexisting Parser and Resolvers

--- src/test/java/com/meti/array/ArrayContentParserTest.java
+++ src/test/java/com/meti/array/ArrayContentParserTest.java
@@ -2,6 +2,9 @@ package com.meti.array;
 
 import com.meti.Compiler;
 import com.meti.*;
+import com.meti.declare.DeclareParser;
+import com.meti.integer.IntParser;
+import com.meti.integer.IntResolver;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,8 +13,15 @@ class ArrayContentParserTest {
 
     @Test
     void parse() {
-        Parser parser = new ParentParser();
-        Resolver resolver = new ParentResolver();
+        Declarations declarations = new Declarations();
+        Parser parser = new ParentParser(
+                new DeclareParser(declarations),
+                new ArrayContentParser(),
+                new IntParser()
+        );
+        Resolver resolver = new ParentResolver(
+                new IntResolver()
+        );
         Compiler compiler = new UnitCompiler(parser, resolver);
         Node node = compiler.parse("val array = Array<Int>{3, 4}");
         assertEquals("int array[]={3,4};", node.render());
