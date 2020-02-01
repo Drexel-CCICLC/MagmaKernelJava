0d21aaa31d8480be8d5b5eb4d92856580142d639
SirMathhman <sirmathhman@gmail.com>
2020-02-01 00:24:59 -0500

    Implement OperationParser

--- src/main/java/com/meti/node/transform/OperationParser.java
+++ src/main/java/com/meti/node/transform/OperationParser.java
@@ -4,12 +4,36 @@ import com.meti.Compiler;
 import com.meti.Parser;
 import com.meti.node.Node;
 
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.Set;
 
 public class OperationParser implements Parser {
+	public static final Set<String> operations = Set.of(
+			"+"
+	);
+
 	@Override
 	public Optional<Node> parse(String content, Compiler compiler) {
 		//TODO: implement OperationParser
-		return Optional.empty();
+
+		return Arrays.stream(Operation.values())
+				.filter(operation -> containsOperation(operation, content))
+				.map(operation -> buildOperation(operation, content, compiler))
+				.findFirst();
+	}
+
+	private boolean containsOperation(Operation operation, String content) {
+		return content.contains(operation.getFrom());
+	}
+
+	private Node buildOperation(Operation operation, String content, Compiler compiler) {
+		String from = operation.getFrom();
+		int fromIndex = content.indexOf(from);
+		String before = content.substring(0, fromIndex).trim();
+		String after = content.substring(fromIndex + 1).trim();
+		Node beforeNode = compiler.parse(before);
+		Node afterNode = compiler.parse(after);
+		return new OperationNode(beforeNode, afterNode, operation);
 	}
 }
