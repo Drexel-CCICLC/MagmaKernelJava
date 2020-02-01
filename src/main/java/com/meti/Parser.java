8a71c8b7a5cf7f205b5fd8c7e12916de92004456
SirMathhman <sirmathhman@gmail.com>
2020-01-31 19:04:54 -0500

    Add OperationParser

--- src/main/java/com/meti/Parser.java
+++ src/main/java/com/meti/Parser.java
@@ -1,10 +1,9 @@
 package com.meti;
 
-import com.meti.exception.ParseException;
 import com.meti.node.Node;
 
 import java.util.Optional;
 
 public interface Parser {
-    Optional<Node> parse(String content, Compiler compiler) throws ParseException;
+    Optional<Node> parse(String content, Compiler compiler);
 }
---
+++ src/main/java/com/meti/node/transform/OperationParser.java
@@ -0,0 +1,15 @@
+package com.meti.node.transform;
+
+import com.meti.Compiler;
+import com.meti.Parser;
+import com.meti.node.Node;
+
+import java.util.Optional;
+
+public class OperationParser implements Parser {
+	@Override
+	public Optional<Node> parse(String content, Compiler compiler) {
+		//TODO: implement OperationParser
+		return Optional.empty();
+	}
+}
