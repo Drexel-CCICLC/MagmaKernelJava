99b2fef7ff4e262af3516c224bcdb56100f4a872
SirMathhman <sirmathhman@gmail.com>
2020-02-01 19:42:48 -0500

    Clean ArrayContentParser

--- src/main/java/com/meti/node/array/ArrayContentParser.java
+++ src/main/java/com/meti/node/array/ArrayContentParser.java
@@ -1,9 +1,9 @@
 package com.meti.node.array;
 
 import com.meti.Compiler;
-import com.meti.node.Node;
 import com.meti.Parser;
 import com.meti.exception.ParseException;
+import com.meti.node.Node;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,22 +12,36 @@ import java.util.stream.Collectors;
 
 public class ArrayContentParser implements Parser {
 	@Override
-	public Optional<Node> parse(String value, Compiler compiler) {
-		//TODO: provide implementation
-		String trim = value.trim();
-		if (trim.startsWith("Array")) {
-			int typeEnd = trim.indexOf('>');
-			String contentString = trim.substring(typeEnd + 1).trim();
-			if (contentString.startsWith("{") && contentString.endsWith("}")) {
-				String content = contentString.substring(1, contentString.length() - 1);
-				List<Node> list = Arrays.stream(content.split(","))
-						.map(compiler::parse)
-						.collect(Collectors.toList());
-				return Optional.of(new ArrayContentNode(list));
-			} else {
-				throw new ParseException(trim + " does not have content surrounded in curly braces.");
-			}
+	public Optional<Node> parse(String content, Compiler compiler) {
+		return Optional.of(content)
+				.map(String::trim)
+				.filter(s -> s.startsWith("Array"))
+				.map(s -> buildNode(s, compiler));
+	}
+
+	private Node buildNode(String trim, Compiler compiler) {
+		String content = parseContent(trim);
+		if (isBlock(content)) {
+			List<Node> list = parseContent(compiler, content);
+			return new ArrayContentNode(list);
+		} else {
+			throw new ParseException(trim + " does not have content surrounded in curly braces.");
 		}
-		return Optional.empty();
+	}
+
+	private String parseContent(String trim) {
+		int typeEnd = trim.indexOf('>');
+		return trim.substring(typeEnd + 1).trim();
+	}
+
+	private boolean isBlock(String contentString) {
+		return contentString.startsWith("{") && contentString.endsWith("}");
+	}
+
+	private List<Node> parseContent(Compiler compiler, String contentString) {
+		String content = contentString.substring(1, contentString.length() - 1);
+		return Arrays.stream(content.split(","))
+				.map(compiler::parse)
+				.collect(Collectors.toList());
 	}
 }
