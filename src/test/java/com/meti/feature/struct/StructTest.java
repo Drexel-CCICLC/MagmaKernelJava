f3b847fc037017d36286d1ca98cf46d615ebdcca
SirMathhman <sirmathhman@gmail.com>
2020-02-03 16:12:13 -0500

    Add StructTest

---
+++ src/test/java/com/meti/feature/struct/StructTest.java
@@ -0,0 +1,52 @@
+package com.meti.feature.struct;
+
+import com.meti.Compiler;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StructTest {
+	@Test
+	void complete() {
+		Compiler compiler = new Compiler();
+		String result = compiler.compile("""
+				val complete = (Int value) => Void :{
+				};
+				            """);
+		assertEquals("void complete(int value){}", result);
+	}
+
+	@Test
+	void content() {
+		Compiler compiler = new Compiler();
+		String result = compiler.compile("""
+				val content = (Int value) => Void :{
+					val x = value;
+				};
+				            """);
+		assertEquals("void content(int value){" +
+				"int x=value;}", result);
+	}
+
+	@Test
+	void multiple() {
+		Compiler compiler = new Compiler();
+		String result = compiler.compile("""
+				val complete = (Int value0, Int value1) => Void :{
+				};
+				            """);
+		assertEquals("void complete(int value0,int value1){}", result);
+	}
+
+	@Test
+	void returnTest() {
+		Compiler compiler = new Compiler();
+		String result = compiler.compile("""
+				val content = (Int value) => Int :{
+					return value;
+				};
+				            """);
+		assertEquals("int content(int value){" +
+				"return value;}", result);
+	}
+}
