1380b2dbe83153e7a3b5fd6ec4d7aa3f93ba0064
SirMathhman <SirMathhman@gmail.com>
2020-02-05 16:24:42 -0500

    Add CharResolver

--- src/main/java/com/meti/core/MagmaCompiler.java
+++ src/main/java/com/meti/core/MagmaCompiler.java
@@ -17,57 +17,58 @@ import com.meti.node.thrower.TryParser;
 import com.meti.node.transform.*;
 
 class MagmaCompiler extends UnitCompiler {
-	MagmaCompiler(Cache cache) {
-		this(cache, new TreeDeclarations());
-	}
+    MagmaCompiler(Cache cache) {
+        this(cache, new TreeDeclarations());
+    }
 
-	private MagmaCompiler(Cache cache, Declarations declarations) {
-		this(declarations, new StructUnit(declarations, cache), cache);
-	}
+    private MagmaCompiler(Cache cache, Declarations declarations) {
+        this(declarations, new StructUnit(declarations, cache), cache);
+    }
 
-	private MagmaCompiler(Declarations declarations, Unit unit, Cache cache) {
-		this(new ParentParser(
-						unit,
-						new WhileParser(),
-						new DereferenceParser(),
-						new IfParser(),
-						new CastParser(),
-						new BlockParser(),
-						new TryParser(),
-						new CatchParser(declarations),
-						new ThrowParser(declarations, cache),
-						new NullParser(),
-						new ReturnParser(),
-						new DeclareParser(declarations),
-						new NotParser(),
-						new ElseParser(),
-						new BooleanParser(),
-						new InvocationParser(declarations),
-						new OperationParser(),
-						new ThisParser(declarations),
-						new IntParser(),
-						new StringParser(),
-						new VariableParser(declarations)
-				),
-				new ParentResolver(
-						unit,
-						new OperationResolver(),
-						new BlockResolver(declarations),
-						new VoidResolver(),
-						new CastResolver(),
-						new BooleanResolver(),
-						new IntResolver(),
-						new AnyResolver(),
-						new PointerResolver(),
-						new InvocationResolver(declarations),
-						new StringResolver(),
-						new FloatResolver(),
-						new VariableResolver(declarations),
-						new ObjectResolver(declarations)
-				));
-	}
+    private MagmaCompiler(Declarations declarations, Unit unit, Cache cache) {
+        this(new ParentParser(
+                        unit,
+                        new WhileParser(),
+                        new DereferenceParser(),
+                        new IfParser(),
+                        new CastParser(),
+                        new BlockParser(),
+                        new TryParser(),
+                        new CatchParser(declarations),
+                        new ThrowParser(declarations, cache),
+                        new NullParser(),
+                        new ReturnParser(),
+                        new DeclareParser(declarations),
+                        new NotParser(),
+                        new ElseParser(),
+                        new BooleanParser(),
+                        new InvocationParser(declarations),
+                        new OperationParser(),
+                        new ThisParser(declarations),
+                        new IntParser(),
+                        new StringParser(),
+                        new VariableParser(declarations)
+                ),
+                new ParentResolver(
+                        unit,
+                        new CharResolver(),
+                        new OperationResolver(),
+                        new BlockResolver(declarations),
+                        new VoidResolver(),
+                        new CastResolver(),
+                        new BooleanResolver(),
+                        new IntResolver(),
+                        new AnyResolver(),
+                        new PointerResolver(),
+                        new InvocationResolver(declarations),
+                        new StringResolver(),
+                        new FloatResolver(),
+                        new VariableResolver(declarations),
+                        new ObjectResolver(declarations)
+                ));
+    }
 
-	private MagmaCompiler(Parser rootParser, Resolver rootResolver) {
-		super(rootParser, rootResolver);
-	}
+    private MagmaCompiler(Parser rootParser, Resolver rootResolver) {
+        super(rootParser, rootResolver);
+    }
 }
