1ccfb2ed3a22bd9487c5d0f397c080b1e70de938
SirMathhman <sirmathhman@gmail.com>
2020-02-06 18:58:01 -0500

    Clean CompileTask

--- src/main/java/com/meti/core/task/CompileTask.java
+++ src/main/java/com/meti/core/task/CompileTask.java
@@ -16,6 +16,9 @@ import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -28,6 +31,11 @@ public class CompileTask implements Task {
 	private final Compiler compiler = new MagmaCompiler(cache, declarations);
 	private final Collection<String> headers = new HashSet<>();
 	private final Logger logger;
+	private final Map<BiPredicate<String, Path>, BiFunction<String, Path, String>> map = Map.of(
+			(s, path) -> isHeader(s), (s, path) -> readHeader(s),
+			(s, path) -> isSource(s), this::readPath,
+			this::isDirectory, this::readDirectory
+	);
 
 	public CompileTask(Logger logger) {
 		this.logger = logger;
@@ -38,26 +46,18 @@ public class CompileTask implements Task {
 		return line.startsWith("compile");
 	}
 
-	private Path ensureBuild(Path directory) throws IOException {
-		Path build = directory.resolve(BUILD);
-		if (!Files.exists(build)) {
-			Files.createFile(build);
-		}
-		return build;
-	}
-
-	private void clearFields() {
-		cache.clear();
-		headers.clear();
-		declarations.clear();
-	}
-
 	@Override
 	public void execute(String line) {
 		logger.log(Level.INFO, "Compiling sources.");
 		clearFields();
 		resetHeaders();
 		processCache();
+	}
+
+	private void clearFields() {
+		cache.clear();
+		headers.clear();
+		declarations.clear();
 	}
 
 	private void resetHeaders() {
@@ -80,16 +80,21 @@ public class CompileTask implements Task {
 		writeCache();
 	}
 
-	private void populateCache(Partitioner partitioner) {
-		populateCacheExceptionally(partitioner);
+	private boolean isDirectory(String childName, Path parent) {
+		Path child = parent.resolve(childName);
+		return Files.isDirectory(child);
 	}
 
-	private void writeCache() {
-		try {
-			writeCacheExceptionally();
-		} catch (IOException e) {
-			logger.log(Level.SEVERE, "Failed to write output.", e);
-		}
+	private boolean isHeader(String s) {
+		return s.endsWith(".h");
+	}
+
+	private boolean isSource(String s) {
+		return s.endsWith(".magma");
+	}
+
+	private void populateCache(Partitioner partitioner) {
+		populateCacheExceptionally(partitioner);
 	}
 
 	private void populateCacheExceptionally(Partitioner partitioner) {
@@ -100,6 +105,14 @@ public class CompileTask implements Task {
 				.forEach(cache::add);
 	}
 
+	private String readDirectory(String trim, Path directory) {
+		try {
+			return readBuild(directory.resolve(trim));
+		} catch (IOException e) {
+			return logInvalidPath(trim, directory, e);
+		}
+	}
+
 	private String readBuild(Path directory) throws IOException {
 		Path build = ensureBuild(directory);
 		List<String> lines = Files.readAllLines(build);
@@ -107,33 +120,64 @@ public class CompileTask implements Task {
 		for (String line : lines) {
 			String trim = line.trim();
 			if (!trim.isBlank()) {
-				if (trim.endsWith(".h")) {
-					readHeader(trim);
-				} else if (trim.endsWith(".magma")) {
-					builder.append(readPath(trim, directory));
-				} else if (Files.isDirectory(directory.resolve(trim))) {
-					builder.append(readDirectory(trim, directory));
-				} else {
-					throw new CompileException("Unknown child: " + trim);
-				}
+				String any = resolveChild(directory, trim);
+				builder.append(any);
 			}
 		}
 		return builder.toString();
 	}
 
-	private String readDirectory(String trim, Path directory) throws IOException {
-		return readBuild(directory.resolve(trim));
+	private String logInvalidPath(String value, Path directory, IOException cause) {
+		String trim = value.trim();
+		Path path = directory.resolve(trim);
+		logger.log(Level.SEVERE, "Failed to read path: " + path, cause);
+		return "";
 	}
 
-	private void readHeader(String value) {
+	private Path ensureBuild(Path directory) throws IOException {
+		Path build = directory.resolve(BUILD);
+		if (!Files.exists(build)) {
+			Files.createFile(build);
+		}
+		return build;
+	}
+
+	private String resolveChild(Path directory, String trim) {
+		return map.keySet()
+				.stream()
+				.filter(predicate -> predicate.test(trim, directory))
+				.map(map::get)
+				.map(function -> function.apply(trim, directory))
+				.findAny()
+				.orElseThrow(() -> new CompileException("Unknown child: " + trim));
+	}
+
+	private String readHeader(String value) {
 		headers.add(value.trim());
+		return "";
 	}
 
-	private String readPath(String value, Path directory) throws IOException {
+	private String readPath(String value, Path directory) {
+		try {
+			return readPathExceptionally(value, directory);
+		} catch (IOException e) {
+			return logInvalidPath(value, directory, e);
+		}
+	}
+
+	private String readPathExceptionally(String value, Path directory) throws IOException {
 		String trim = value.trim();
 		Path path = directory.resolve(trim);
 		List<String> lines = Files.readAllLines(path);
 		return String.join("", lines);
+	}
+
+	private void writeCache() {
+		try {
+			writeCacheExceptionally();
+		} catch (IOException e) {
+			logger.log(Level.SEVERE, "Failed to write output.", e);
+		}
 	}
 
 	private void writeCacheExceptionally() throws IOException {
