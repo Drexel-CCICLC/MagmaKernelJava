1da300f9c7c2d721b7df75ebbc87a4872a4d936c
SirMathhman <sirmathhman@gmail.com>
2020-02-07 17:50:46 -0500

    Clean ReferenceParser

--- src/main/java/com/meti/node/primitive/bool/BooleanParser.java
+++ src/main/java/com/meti/node/primitive/bool/BooleanParser.java
@@ -7,14 +7,14 @@ import com.meti.node.Node;
 import java.util.Optional;
 
 public class BooleanParser implements Parser {
-    @Override
-    public Optional<Node> parse(String content, Compiler compiler) {
-        String trim = content.trim();
-        if (trim.equals("true")) {
-            return Optional.of(new BooleanNode(true));
-        } else if (trim.equals("false")) {
-            return Optional.of(new BooleanNode(false));
-        }
-        return Optional.empty();
-    }
+	@Override
+	public Optional<Node> parse(String content, Compiler compiler) {
+		String trim = content.trim();
+		if ("true".equals(trim)) {
+			return Optional.of(new BooleanNode(true));
+		} else if ("false".equals(trim)) {
+			return Optional.of(new BooleanNode(false));
+		}
+		return Optional.empty();
+	}
 }
