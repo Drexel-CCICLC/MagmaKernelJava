9ccd8f2c7ca668965c8f44852eaaffb1748ad199
SirMathhman <SirMathhman@gmail.com>
2020-02-07 13:33:44 -0500

    Clean DeclareNode

--- src/main/java/com/meti/node/declare/DeclareNode.java
+++ src/main/java/com/meti/node/declare/DeclareNode.java
@@ -4,25 +4,28 @@ import com.meti.node.Node;
 import com.meti.node.Type;
 
 public class DeclareNode implements Node {
-	private final String name;
-	private final Type type;
-	private final Node value;
+    private final String name;
+    private final Type type;
+    private final Node value;
 
-	public DeclareNode(Type type, String name, Node value) {
-		this.value = value;
-		this.type = type;
-		this.name = name;
-	}
+    public DeclareNode(Type type, String name, Node value) {
+        this.value = value;
+        this.type = type;
+        this.name = name;
+    }
 
-	@Override
-	public String render() {
-		if (null != value) {
-			String renderedValue = value.render();
-			return renderedValue.isBlank() ? "" :
-					type.render(name) + "=" +
-							renderedValue + ";";
-		} else {
-			return type.render(name) + ";";
-		}
-	}
+    @Override
+    public String render() {
+        return null == value ? renderWithoutValue() : renderWithValue(value);
+    }
+
+    private String renderWithoutValue() {
+        return type.render(name) + ";";
+    }
+
+    private String renderWithValue(Node value) {
+        String renderedValue = value.render();
+        return renderedValue.isBlank() ? "" :
+                type.render(name) + "=" + renderedValue + ";";
+    }
 }
